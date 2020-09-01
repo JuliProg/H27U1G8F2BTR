41918dbab7433e31e268f8c1a01818bef2b9485b
JuliProg <juliprog1110.gmail.com>
2020-09-01 11:01:09 +0000

    automated add new chip

--- H27U1G8F2BTR/Properties/AssemblyInfo.cs
+++ H27U1G8F2BTR/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("K9F1G08U0D")]
+[assembly: AssemblyTitle("H27U1G8F2BTR")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("K9F1G08U0D")]
+[assembly: AssemblyProduct("H27U1G8F2BTR")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
